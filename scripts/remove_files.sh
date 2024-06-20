188ff7064571c2ad11dd9fb25233bc15706acda4
Utkarsh Raj <65725785+UtkarshRaj0602@users.noreply.github.com>
2024-06-20 12:40:57 +0530

    Update remove_files.sh

--- scripts/remove_files.sh
+++ scripts/remove_files.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /var/www/html/
 rm -rf *
+sudo chmod -R 755 /opt/codedeploy-agent/deployment-root/
+sudo chown -R codedeploy-user:codedeploy-user /opt/codedeploy-agent/deployment-root/
+
