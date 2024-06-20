6f77e2840be60dd33dc80f676ad34939d6911f40
Utkarsh Raj <65725785+UtkarshRaj0602@users.noreply.github.com>
2024-06-20 15:15:03 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-rm -rf /var/www/html/*
-rm -rf /var/www/html/.*
-sudo service codedeploy-agent stop
-rm -rf /opt/codedeploy-agent/deployment-root/*
-sudo service codedeploy-agent start
+sudo service codedeploy-agent restart
